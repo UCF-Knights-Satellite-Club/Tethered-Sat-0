fd9c065d932f06a3963bf84d8cf73576a393bf6d
Garrett Webb <webbgamers@gmail.com>
2024-11-18 15:23:28 -0500

    led indicate software reset

--- software/testing/servo_test/servo_test.ino
+++ software/testing/servo_test/servo_test.ino
@@ -10,6 +10,14 @@ void setup() {
   servo.attach(SERVO_PIN);
   servo.write(SERVO_STOW_POS);
 
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  
+  delay(2000);
+
+  digitalWrite(LED_BUILTIN, LOW);
+
   delay(2000);
 
   servo.write(SERVO_DEPLOY_POS);
